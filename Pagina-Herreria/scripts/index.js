9e8356e465cbab5118b727db739b712487eb0ab9
DanielRodriguez <dany.15.dars@gmail.com>
2025-02-20 21:50:48 -0800

    Se suben cambios comentados en la sesion

--- Pagina-Herreria/scripts/index.js
+++ Pagina-Herreria/scripts/index.js
@@ -3,12 +3,8 @@ const menu = document.querySelector("#Menu");
 const btnAbrir = document.querySelector("#btnAbrir");
 const btnCerrar = document.querySelector("#btnCerrar");
 //Eventos para abrir y cerrar
-btnAbrir.addEventListener("click", ()=>{
-    console.log('enter a abrir');
-    menu.classList.add("visible");
-});
+btnAbrir.addEventListener("click", () => {
+  console.log("enter a abrir");
 
-btnCerrar.addEventListener("click", () =>{
-    console.log('enter a cerrar');
-    menu.classList.remove("visible");
+  menu.classList.toggle("visible");
 });
